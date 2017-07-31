609b87e8e984c2834cc0fb82c117a63d19c05385
wangpei <wangpei163@vip.qq.com>
2017-07-31 23:17:29 +0800

    add views in models

--- blog/models.py
+++ blog/models.py
@@ -59,7 +59,12 @@ class Post(models.Model):
     # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
     author = models.ForeignKey(User)
-
+    #新增views字段记录阅读量
+    views = models.PositiveIntegerField(default=0)
+    #增加模型方法,先将自身对应的views字段值加一，再save到数据库
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
     #reverse 函数，它的第一个参数的值是 'blog:detail'，意思是 blog 应用下的 name=detail 的函数，由于我们在上面通过 app_name = 'blog' 告诉了 Django 这个 URL 模块是属于 blog 应用的，因此 Django 能够顺利地找到 blog 应用下 name 为 detail 的视图函数，于是 reverse 函数会去解析这个视图函数对应的 URL，我们这里 detail 对应的规则就是 post/(?P<pk>[0-9]+)/ 这个正则表达式，而正则表达式部分会被后面传入的参数 pk 替换，所以，如果 Post 的 id（或者 pk，这里 pk 和 id 是等价的） 是 255 的话，那么 get_absolute_url 函数返回的就是 /post/255/ ，这样 Post 自己就生成了自己的 URL。
     def get_absolute_url(self):
         return reverse('blog:detail',kwargs={'pk':self.pk})
