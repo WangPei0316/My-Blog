ef4f3898729f5ea01aa270725f11436df8a4b207
wangpei <wangpei163@vip.qq.com>
2017-08-01 12:27:28 +0800

    change excerpt

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.html import strip_tags
+import markdown
 
 
 class Category(models.Model):
@@ -32,13 +34,13 @@ class Post(models.Model):
     #标题
     title = models.CharField(max_length=70)
 
-    # 文章正文，使用 TextField
-    # 存储比较短的字符串可以使用 CharField，但对于文章的正文来说可能会是一大段文本，因此使用 TextField 来存储大段文本。
-    body = models.TextField()
-
     #创建时间和最后一次修改时间.使用DateTimeField()
     create_time = models.DateTimeField()
     modified_time = models.DateTimeField()
+
+    # 文章正文，使用 TextField
+    # 存储比较短的字符串可以使用 CharField，但对于文章的正文来说可能会是一大段文本，因此使用 TextField 来存储大段文本。
+    body = models.TextField()
 
     # 文章摘要，可以没有文章摘要，但默认情况下 CharField 要求我们必须存入数据，否则就会报错。
     # 指定 CharField 的 blank=True 参数值后就可以允许空值了。
@@ -65,10 +67,25 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1
         self.save(update_fields=['views'])
-    #reverse 函数，它的第一个参数的值是 'blog:detail'，意思是 blog 应用下的 name=detail 的函数，由于我们在上面通过 app_name = 'blog' 告诉了 Django 这个 URL 模块是属于 blog 应用的，因此 Django 能够顺利地找到 blog 应用下 name 为 detail 的视图函数，于是 reverse 函数会去解析这个视图函数对应的 URL，我们这里 detail 对应的规则就是 post/(?P<pk>[0-9]+)/ 这个正则表达式，而正则表达式部分会被后面传入的参数 pk 替换，所以，如果 Post 的 id（或者 pk，这里 pk 和 id 是等价的） 是 255 的话，那么 get_absolute_url 函数返回的就是 /post/255/ ，这样 Post 自己就生成了自己的 URL。
+    #reverse 函数，第一个参数的值是 'blog:detail'，是 blog下的 name=detail 的函数，由于在上面通过 app_name = 'blog' 告诉了 Django 这个 URL 模块是属于 blog 应用的，因此 Django 能够顺利地找到 blog 应用下 name 为 detail 的视图函数，于是 reverse 函数会去解析这个视图函数对应的 URL，这里 detail 对应的规则就是 post/(?P<pk>[0-9]+)/ 这个正则表达式，而正则表达式部分会被后面传入的参数 pk 替换，所以，如果 Post 的 id（或者 pk，这里 pk 和 id 是等价的） 是 255 的话，那么 get_absolute_url 函数返回的就是 /post/255/ ，这样 Post 自己就生成了自己的 URL。
     def get_absolute_url(self):
         return reverse('blog:detail',kwargs={'pk':self.pk})
     def __str__(self):
         return self.title
     class Meta:
         ordering = ['-create_time']
+
+    def save(self, *args,**kwargs):
+        #如果没有填写摘要
+        if not self.excerpt:
+            #先实例化一个Markdown类，用于渲染body的文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            #先将Markdown文本渲染成html文本
+            #strip_tags去掉HTML文本的全部HTML标签
+            #从文本摘取前54个字符赋给excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        #调用父类的save方法将数据保存到数据库中
+        super(Post,self).save(*args,**kwargs)
